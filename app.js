5b14c801502c37e09d1a7f9d8fa23b21240792f8
ArturTrube <90390837+ArturTrube@users.noreply.github.com>
2024-05-20 09:02:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 const session = require('express-session');
 
-const url = 'mongodb+srv://VladislavSupilov:koleso777@vladcluster.jembrjv.mongodb.net/?retryWrites=true&w=majority&appName=vladcluster';
+const url = 'mongodb+srv://VladislavSupilov:koleso777@vladcluster.jembrjv.mongodb.net/kodumoobel?retryWrites=true&w=majority&appName=vladcluster';
 
 mongoose.connect(url);
 
